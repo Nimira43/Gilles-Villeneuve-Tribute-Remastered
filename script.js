3feb84e313c124639f4293dd0f77292ec323a44a
Nick Rabone <n.rabone72@aol.com>
2024-07-31 20:15:42 +0100

    Updated Javascript

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const bgImageEl = document.getElementById("bg-image")
-window.addEventListener("scroll", () =>  {
-    updateImage()
-})
-
-function updateImage() {
-    bgImageEl.style.opacity = 1 - window.pageYOffset / 900
-    bgImageEl.style.backgroundSize = 120 - window.pageYOffset / 120 + "%"
+const updateImage = () => {
+    bgImageEl.style.opacity = 1 - window.scrollY / 900
+    bgImageEl.style.backgroundSize = 120 - window.scrollY / 120 + "%"
 }
+
+const bgImageEl = document.getElementById("bg-image")
+window.addEventListener("scroll", () => updateImage())
+
